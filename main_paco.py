abf5edc828b6702c9b90197ae01db25629ebc964
LDemange <demange.louis@hotmail.fr>
2024-07-09 22:02:23 +0200

    squelette

--- main_paco.py
+++ main_paco.py
@@ -49,6 +49,13 @@ class Case:
     def placerPion(self):
         if self.pion:
             can.create_oval(self.x1+10, self.y1+10, self.x2-10, self.y2-10, fill = self.couleurPion)
+            
+    def peutBougerVers(self, dest):
+        caseDest = trouverCase(dest)
+        if not caseDest:
+            return 0
+        else:
+            return 0 # A terminer
     """
     Purpose: 
     """
@@ -92,6 +99,45 @@ def laby():
 #initialisation des variables
 x1, y1, x2, y2  = 5, 5, 45, 45
 tout_les_cases = []
+session = ''
+
+def trouverCase(coord):
+    for case in tout_les_cases:
+        if case.x1 == coord[0] and case.y1 == coord[1] and case.x2 == coord[2] and case.y2 == coord[3]:
+            return case
+    return 0
+
+def click(event):
+    global caseDepart, pionClicker, session
+    x, y = event.x, event.y 
+    pionClicker = 0
+    caseDepart = 0
+    clicker = can.find_overlapping(x,y,x,y)
+    if len(clicker) > 1:
+        coord = can.coords(clicker[0])
+        caseDepart = trouverCase(coord)
+        if caseDepart.couleurPion == session:
+            pionClicker = 0
+        else:
+            pionClicker = clicker[1]
+        
+def bouger(event):
+    global caseDepart, pionClicker
+    x, y = event.x, event.y
+    if caseDepart and pionClicker:
+        coord = can.coords(pionClicker)
+        deplacement = [[x-10, y-10,x+10,y+10]]
+        can.coords(pionClicker, deplacement[0])
+
+def arret(event):
+    global caseDepart, pionClicker, scoreV, scoreJ, session
+    x,y = event.x, event.y
+    
+    '''collision = find_overlapping(x-10,y-10,x+10,y+10)
+    coord = can.coords(collision[0])
+    
+    if not caseDepart.peutBougerVers(coord):
+        can.coords(pionClicker, caseDepart,getCoordCase()) A terminer''' 
 
 fen = Tk()
 fen.title('labyrinthe')
@@ -107,10 +153,10 @@ score = Label(fen, text = 'V : 0 vs J : 0', font = font, fg ='white', bg ='#f00a
 can.pack()
 bouttonLaby.pack()
     
-''' can.bind('ButtonPress-1', click)
-can.bind('B1-motion', bouger)
-can.bind('ButtonRelease-1', arret)
-can.configure(cursor = 'hand2')'''
+can.bind('<ButtonPress-1>', click)
+can.bind('<B1-Motion>', bouger)
+can.bind('<ButtonRelease-1>', arret)
+can.configure(cursor = 'hand2')
     
 fen.resizable(False,False)
 fen.mainloop()
